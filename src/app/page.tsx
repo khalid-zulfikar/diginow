57eee22322d71f0a5c6750fddd4154dc52db0924
Muhamad Zulfikar <muhamadzulfikar22@gmail.com>
2025-09-15 14:50:44 +0700

    Add Darkmode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from '@/components/ui/button';
+import { DarkmodeToggle } from '@/components/ui/common/darkmode-toggle';
 import { Input } from '@/components/ui/input';
 
 export default function Home() {
   return (
     <div>
       <Input />
-      <Button>Hello</Button>
+      <Button className="bg-red-400 dark:bg-amber-300">Hello</Button>
+      <DarkmodeToggle />
     </div>
   );
 }
